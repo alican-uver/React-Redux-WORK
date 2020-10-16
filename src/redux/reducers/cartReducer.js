adade9d41a11da9bd779011bb26c44689656c6d5
Alican Uver <alican.uver@emlakjet.com>
2020-10-16 23:23:02 +0300

    addToCart func written

---
+++ src/redux/reducers/cartReducer.js
@@ -0,0 +1,23 @@
+import * as actionTypes from "../actions/actionTypes";
+import initialState from "./initialState";
+
+export default function cartReducer(state = initialState.cart, action) {
+  switch (action.type) {
+    case actionTypes.ADD_TO_CART:
+      let sameItem = state.find(c => c.product.id === action.payload.product.id);
+        if (sameItem) {
+            let newState = state.map(cartItem => {
+                if  (cartItem.product.id === action.cartItem.product.id) {
+                    return Object.assign({}, sameItem, {quantity: sameItem.quantity + 1})
+                }
+                return cartItem;
+            })
+            return newState;
+        }
+        else {
+            return [...state, action.payload]
+        }
+    default:
+      return state;
+  }
+}
