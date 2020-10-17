860e71009f38cb8caa8e8be7da621de4e58b2a80
Alican Uver <alican.uver@emlakjet.com>
2020-10-17 20:25:15 +0300

    more effective code written

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -4,21 +4,21 @@ import initialState from "./initialState";
 export default function cartReducer(state = initialState.cart, action) {
   switch (action.type) {
     case actionTypes.ADD_TO_CART:
-      console.log(action)
-      console.log(state)
       let sameItem = state.find(c => c.product.id === action.payload.product.id);
         if (sameItem) {
-            let newState = state.map(cartItem => {
-                if  (cartItem.product.id === action.payload.product.id) {
-                    return Object.assign({}, sameItem, {quantity: sameItem.quantity + 1})
-                }
-                return cartItem;
-            })
-            return newState;
+          let newState = [...state] ;
+          newState.map(cartItem => {
+            if (cartItem.product.id === action.payload.product.id) {
+              cartItem.quantity += 1
+            }
+            return cartItem;
+          })
+          return [...newState];
         }
         else {
             return [...state, action.payload]
-        }
+        };
+    // eslint-disable-next-line no-fallthrough
     default:
       return state;
   }
